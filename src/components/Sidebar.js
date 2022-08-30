381792e5d286067f3cb22959c14ad3621ae1d84b
Ben Chau <78128664+ben842002@users.noreply.github.com>
2022-08-30 19:57:03 -0400

    fetch RapidAPI YouTube data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = "New";
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
     <Stack
         direction="row" 
         sx={{
@@ -13,9 +11,12 @@ const Sidebar = () => (
             flexDirection: { md: 'column' },
         }}    
     >
+
+    {/* display category buttons */}
     {categories.map((category) => (
         <button 
             className="category-btn"
+            onClick={() => setSelectedCategory(category.name)}
             style={{
                 background: category.name === selectedCategory && "#FC1503",
                 color: "#fff"
